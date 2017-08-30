cf2dd9e07af5b6c92187f56733043eb403fb377c
Cameron Loader <cameron@loaderusa.net>
2017-08-30 04:24:26 +0000

    added interfaces config/added scenario to bootstrap-aio (RPC)

--- scripts/deploy-with-ironic.sh
+++ scripts/deploy-with-ironic.sh
@@ -28,12 +28,24 @@ source ${BASE_DIR}/scripts/functions.sh
 # Check the openstack-ansible submodule status
 check_submodule_status
 
-# begin the bootstrap process
-pushd ${OA_DIR}
-    ${BASE_DIR}/openstack-ansible/scripts/bootstrap-ansible.sh
-    ${BASE_DIR}/openstack-ansible/scripts/bootstrap-aio.sh
-    openstack-ansible ${RPCD_DIR}/playbooks/ironic-pre-config.yml
-    ${BASE_DIR}/scripts/deploy.sh
-    openstack-ansible ${RPCD_DIR}/playbooks/ironic-post-install-config.yml
-    openstack-ansible ${RPCD_DIR}/openstack/playbooks/os-ironic-install.yml
-popd
+export DEPLOY_IRONIC="yes"
+export DEPLOY_AIO="yes"
+# begin the ansible setup
+${BASE_DIR}/scripts/bootstrap-ansible.sh
+${BASE_DIR}/scripts/bootstrap-aio.sh
+sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/openstack_user_config.yml
+sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/conf.d/*.yml
+# drop interface config for ironic
+openstack-ansible ${BASE_DIR}/scripts/setup-ironic-networking.yml
+${BASE_DIR}/scripts/bootstrap-aio.sh
+# replace aio1 with the hostname
+sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/openstack_user_config.yml
+sed -i "s/aio1/$(hostname)/" /etc/openstack_deploy/conf.d/*.yml
+# run pre-install config for ironic
+openstack-ansible ${RPCD_DIR}/playbooks/ironic-pre-config.yml
+# deploy RPC
+${BASE_DIR}/scripts/deploy.sh
+# do post ironic install config
+openstack-ansible ${RPCD_DIR}/playbooks/ironic-post-install-config.yml
+# re-run ironic install to include the post-config
+openstack-ansible ${RPCD_DIR}/openstack/playbooks/os-ironic-install.yml
